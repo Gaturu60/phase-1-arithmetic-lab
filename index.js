0bd7ef4e46bafa8ba13b986d06afedddbcae4f4d
Dennis Gaturu <gaturugaturu60@gmail.com>
2024-07-04 14:23:40 +0300

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1= 62
+let num2= 1
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor (Math.random() *Number.MAX_SAFE_INTEGER) +1;
+
+let num3 = 14
+let num4 = 10
+let mod = num3 % num4;
+
+let numbers = [3, 5, 1, 10, 20];
+let max = Math.max(...numbers);
